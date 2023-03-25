3d4feec18b12ee163d3148ae447b93aa165fa6f9
Sejal <21bec046@nith.ac.in>
2023-03-26 01:04:04 +0530

    move: Navigation fragments

--- app/src/main/java/com/example/healthcare/fragments/navFragment/infoFragment.kt
+++ app/src/main/java/com/example/healthcare/fragments/navFragment/infoFragment.kt
@@ -1,4 +1,4 @@
-package com.example.healthcare.fragments
+package com.example.healthcare.fragments.navFragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [calenderFragment.newInstance] factory method to
+ * Use the [infoFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class calenderFragment : Fragment() {
+class infoFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class calenderFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calender, container, false)
+        return inflater.inflate(R.layout.fragment_info, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class calenderFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment calenderFragment.
+         * @return A new instance of fragment infoFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            calenderFragment().apply {
+            infoFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/example/healthcare/fragments/navFragment/receiptFragment.kt
+++ app/src/main/java/com/example/healthcare/fragments/navFragment/receiptFragment.kt
@@ -1,4 +1,4 @@
-package com.example.healthcare.fragments
+package com.example.healthcare.fragments.navFragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
